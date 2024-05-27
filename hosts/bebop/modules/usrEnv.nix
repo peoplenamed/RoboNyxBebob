dc46a97a875172799781dd963049ad86704227e7
nerd <peter0x000@gmail.com>
2024-05-27 15:31:14 -0400

    switched to hyprpaper

--- hosts/bebop/modules/usrEnv.nix
+++ hosts/bebop/modules/usrEnv.nix
@@ -1,11 +1,13 @@
 {
   modules.usrEnv = {
     desktop = "Hyprland";
+    wallpaperService = "hyprpaper";
     wayland = {
       statusBar = {
         waybar.enable = true;
       };
     };
+
     programs.screenlock.swaylock.enable = true;
   };
 }
